e2e57c7a0ead25361cf3ffe6622c4ed71ee77ceb
Brendan Hanna and Justin Gomez <brendan@>
2017-04-27 10:20:03 -0700

    add background image, jumbotron, form, javascript that takes the input from the form and uses and if else statement to display one of two divs with corresponding content

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
-//business logic
-
+// business logic
+// var age = parseInt(("what is your age?"));
 
 //user interface logic
-$(document).ready(function(){
-  alert("hi!")
+$(document).ready(function() {
+
+  $("form#userForm").submit(function(event) {
+    event.preventDefault();
+    var age = parseInt($("#voter").val());
+
+    if (age > 18) {
+      $('#ofAge').show();
+      $('#underAge').hide();
+
+    } else if (age < 18) {
+      $('#underAge').show();
+      $('#ofAge').hide();
+    }
+
+  });
+
+
+
+
 });
